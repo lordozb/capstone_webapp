3fb4863aa6a52e7c3a6f210e986f490171fa9e66
lordozb <aadityaja@gmail.com>
2018-11-18 14:07:06 +0530

    Added analyzes

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -1,18 +1,21 @@
 class AnalyzeController < ApplicationController
 
 	def index
-
+		@video = Video.all
 	end
 
 	def create
-		@video = Video.find(params[:video_id])
-		@analyze = @video.analyzes.create(params[:analyze].permit(:file_location))
-		redirect_to video_analyze_path(@video,@analyze)
+		begin
+			@video = Video.find(params[:video_id])
+			@analyze = @video.analyzes.create(params[:analyze].permit(:file_location))
+			redirect_to video_analyze_path(@video,@analyze)
+		rescue => error
+			redirect_to @video
+		end
 	end
 
 	def show	
 		@analyze = Analyze.find(params[:id])
-		`python ./public/scripts/plot.py ./public#{@analyze.file_location.url} ./public/data/#{@analyze[:video_id]}/data.txt`
-
+		`python ./public/scripts/plot.py ./public#{@analyze.file_location.url} ./public/data/#{@analyze[:video_id]}/data.txt #{@analyze[:video_id]}`
 	end
 end
